c1f471e42b9b218c9fe4b078589846372d3337f6
ros <ros>
2022-05-20 01:44:21 -0700

    repo init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/rosForIfly/src
+# Build directory: /home/ros/rosForIfly/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("gazebo_map")
+subdirs("gazebo_nav")
+subdirs("gazebo_pkg")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ros/rosForIfly/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ros/rosForIfly/devel;/opt/ros/melodic'.split(';') if '/home/ros/rosForIfly/devel;/opt/ros/melodic' != '' else []
